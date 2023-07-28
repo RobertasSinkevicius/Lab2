ea0e64a611a40ffbd2949a154b48aea5cc3f82b3
RobertasSinkevicius <Robertas.sinkevicius@gmail.com>
2023-07-28 10:27:24 +0000

    using recursive function

--- main.cpp
+++ main.cpp
@@ -1,25 +1,29 @@
 #include "mbed.h"
+// main() runs in its own thread in the OS
+
 // 0, 1, 1, 2, 3, 5, 8, 13.
+// n=1---0
+// n=2---1
+// n=3---1
+// n=4---2
+
+int fib(int n){
+    if(n == 1)
+        return 0;
+    if(n == 2)
+        return 1;
+    else
+        return (fib(n-1) + fib(n-2));
+
+}
 
 int main()
 {
     printf("Lab 2 program \r\n");
     int max_term = 10;
-    int term_1 = 0;
-    int term_2 = 1;
-    int next_term = 0;
-    for(int i=1; i<max_term; i++){
-        if(i == 1)
-            printf("%d, ", term_1);
-        if(i == 2)
-            printf("%d, ", term_2);
-        else{
-            /// 3rd term ++ prev terms
-            next_term = term_1 + term_2;
-            term_1 = term_2;
-            term_2 = next_term;
-            printf("%d, ", next_term);
-        }
+
+    for(int i=1; i<=max_term; i++){
+        printf("%d, ", fib(i));
     }
     printf("\r\n");
     while (true) {
